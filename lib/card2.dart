69d060f01a9094cb238bfd92b5bec9f7e92e3aaf
doanthekhoinguyen <doanthekhoinguyen.dev@gmail.com>
2023-06-09 20:55:29 +0700

    colompleted

--- lib/card2.dart
+++ lib/card2.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'author_card.dart';
+import 'fooderlich_theme.dart';
 
 class Card2 extends StatelessWidget {
   const Card2({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Center(
-// 1
       child: Container(
         constraints: const BoxConstraints.expand(
           width: 350,
@@ -20,11 +21,39 @@ class Card2 extends StatelessWidget {
             Radius.circular(10.0),
           ),
         ),
-// 2
         child: Column(
           children: [
+            const AuthorCard(
+              authorName: 'Mike Katz',
+              title: 'Smoothie Connoisseur',
+              imageProvider: AssetImage('assets/author_katz.jpeg'),
+            ),
 // TODO 1: add author information
-// TODO 4: add Positioned text
+            Expanded(
+              child: Stack(
+                children: [
+                  Positioned(
+                    bottom: 16,
+                    right: 16,
+                    child: Text(
+                      'Recipe',
+                      style: FooderlichTheme.lightTextTheme.headline1,
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 70,
+                    left: 16,
+                    child: RotatedBox(
+                      quarterTurns: 3,
+                      child: Text(
+                        'Smoothies',
+                        style: FooderlichTheme.lightTextTheme.headline1,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
